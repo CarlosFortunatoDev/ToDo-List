f33403e6c0fe6a6df0859e0927423bd5aa94f4bd
CarlosFortunatoDev <carlosfortunatodev@gmail.com>
2023-05-24 03:23:45 -0300

    Criando o primeiro evento com o todoForm para adicionar novos conteúdos na lista através da função saveTodo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,42 @@ const editForm = document.querySelector('#edit-form')
 const editInput = document.querySelector('#edit-input')
 const cancelEditBtn = document.querySelector('#cancel-edit-btn')
 
+// Funções
+const saveTodo = (text) =>{
+    const todo = document.createElement('div') /*Criando um elemento div*/
+    todo.classList.add('todo') /*Adicionando a classe todo*/
+
+    const todoTitle = document.createElement('h3') /*Criando um elemento h3*/
+    todoTitle.innerText = text; /*Adicionando o texto que veio pelo parâmetro no todoTitle*/
+    todo.appendChild(todoTitle) /*Adicionando o todoTitle dentro do todo*/
+
+    const doneBtn = document.createElement('button') /*Criando um elemento button*/
+    doneBtn.classList.add('finish-todo') /*Adicionando a classe finish-todo*/
+    doneBtn.innerHTML = `<i class="fa-solid fa-check"></i>` /*Adicionando uma tag HTML dentro do doneBtn*/
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement('button') /*Criando um elemento button*/
+    editBtn.classList.add('edit-todo') /*Adicionando a classe edit-todo*/
+    editBtn.innerHTML = `<i class="fa-solid fa-pen"></i>` /*Adicionando uma tag HTML dentro do editBtn*/
+    todo.appendChild(editBtn)
+
+    const deleteBtn = document.createElement('button') /*Criando um elemento button*/
+    deleteBtn.classList.add('remove-todo') /*Adicionando a classe remove-todo*/
+    deleteBtn.innerHTML = `<i class="fa-solid fa-xmark"></i>` /*Adicionando uma tag HTML dentro do deleteBtn*/
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo)
+
+    todoInput.value = '';
+}
+
+// Eventos
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault(); /*Serve para não enviar o formulário para o backend*/
+
+    const inputValue = todoInput.value; /*Salvando o valor do todoInput na variável inputValue*/
+
+    if (inputValue) {
+        saveTodo(inputValue); /*Chamando a função saveTodo passando como parâmetro a variável inputValue*/
+    }
+})
